5a98d0ae363659579859a87c84d192cf466f632b
Yaroslav <122485322+Yaroslav1403@users.noreply.github.com>
2024-10-08 11:37:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+# Пользователь должен ввести значения дня и месяца
 day = int(input("Введите день (число): "))
 month = int(input("Введите месяц (число): "))
-if (month == 3 and day >= 1) or (month in [4, 5]) or (month == 6 and day < 1):
-    season = "Весна"
-elif (month == 6 and day >= 1) or (month in [7, 8]) or (month == 9 and day < 1):
-    season = "Лето"
-elif (month == 9 and day >= 1) or (month in [10, 11]) or (month == 12 and day < 1):
-    season = "Осень"
-elif (month == 12 and day >= 1) or (month in [1, 2]) or (month == 2 and day < 1):
-    season = "Зима"
+# Проверим на отрицательный день или месяц
 if day < 1 or month < 1 or month > 12:
-    print("Некорректный ввод даты.")
-if day > day in [month - 1]:
-        print("Некорректный ввод даты.")
+        print("Введена неккоректная дата.")
+else:
+# Определяем количество дней в каждом месяце года
+    days_in_all_months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+if day > days_in_all_months [month - 1]:
+        print("Введена неккоректная дата.")
+else:
+# Проверяем, к какому времени года относится дата, введённая пользователем   
+    if (month == 3 and day >= 1) or (month in [4, 5]) or (month == 6 and day < 1):
+        season = "Весна"
+    elif (month == 6 and day >= 1) or (month in [7, 8]) or (month == 9 and day < 1):
+        season = "Лето"
+    elif (month == 9 and day >= 1) or (month in [10, 11]) or (month == 12 and day < 1):
+        season = "Осень"
+    elif (month == 12 and day >= 1) or (month in [1, 2]) or (month == 2 and day < 1):
+        season = "Зима"
 print(f"Дата: {day}/{month} - это {season}")
