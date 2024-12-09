8239a72b3d9058ab7418587bdae67676d88652c8
Yaroslav <122485322+Yaroslav1403@users.noreply.github.com>
2024-12-09 10:28:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ if day > days_in_all_months [month - 1]:
 else:
 #Проверяем, к какому времени года относится дата, введённая пользователем   
     if (month == 3 and day >= 1) or (month in [4, 5]) or (month == 6 and day < 1):
-        season = "Весна"
+        season = "весна"
     elif (month == 6 and day >= 1) or (month in [7, 8]) or (month == 9 and day < 1):
-        season = "Лето"
+        season = "лето"
     elif (month == 9 and day >= 1) or (month in [10, 11]) or (month == 12 and day < 1):
-        season = "Осень"
+        season = "осень"
     elif (month == 12 and day >= 1) or (month in [1, 2]) or (month == 2 and day < 1):
-        season = "Зима"
+        season = "зима"
 #Выводим результат
 print(f"Дата: {day}/{month} - это {season}")
